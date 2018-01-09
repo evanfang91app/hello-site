f31b94bf7988860fb8c463097685b35ff0983e04
EvanFang <evanfang@nine-yi.com>
2018-01-09 15:07:02 +0800

    Add xunit test project.

---
+++ HelloSite.XUnit.Test/Test1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HelloSite.XUnit.Test
+{
+    public class Test1
+    {
+        [Fact]
+        public void Test_Method_Hello_World()
+        {
+            Assert.True(true);
+        }
+    }
+}
